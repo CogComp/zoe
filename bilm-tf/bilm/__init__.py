f43a1cae16da2eaf729d6fb6060c8f305fa35142
Xuanyu Zhou <zhouxuanyu.personal@gmail.com>
2018-08-10 11:01:55 +0800

    init adding elmo

---
+++ bilm-tf/bilm/__init__.py
@@ -0,0 +1,6 @@
+
+from .data import Batcher, TokenBatcher
+from .elmo import weight_layers
+from .model import BidirectionalLanguageModel, dump_token_embeddings, \
+    dump_bilm_embeddings, dump_bilm_embeddings_inner
+
---
+++ elmo_processor.py
@@ -0,0 +1,40 @@
+import os
+
+from bilm import dump_bilm_embeddings_inner
+
+
+def vec2string(input_vec, divident):
+    ret = ""
+    for elem in input_vec:
+        elem /= divident
+        ret += str(elem) + ","
+    ret = ret[:-1]
+    return ret
+
+
+def generate_embedding(sentence):
+    FREQ_MAP = {}
+
+    tokens = sentence.split()
+
+    for i in range(0, len(tokens)):
+        key = tokens[i]
+        if key in FREQ_MAP:
+            FREQ_MAP[key] = FREQ_MAP[key] + 1.0
+        else:
+            FREQ_MAP[key] = 1.0
+
+    datadir = os.path.join('bilm-tf', 'model')
+    vocab_file = os.path.join(datadir, 'vocab_test.txt')
+    options_file = os.path.join(datadir, 'options.json')
+    weight_file = os.path.join(datadir, 'elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5')
+
+    embedding = dump_bilm_embeddings_inner(
+        vocab_file, sentence, options_file, weight_file
+    )
+    return embedding
+
+
+sentence = "I like partying."
+print(generate_embedding(sentence))
+
